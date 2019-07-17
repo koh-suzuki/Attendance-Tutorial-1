61091bb0463a75cef00a6fd150f0262bef489d52
koh-suzuki <k.s_coward_0629@icloud.com>
2019-07-17 09:57:05 +0000

    9. 1 「department（所属）」カラム追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,7 +54,7 @@ class UsersController < ApplicationController
     end
    
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
     end
     
      def logged_in_user
